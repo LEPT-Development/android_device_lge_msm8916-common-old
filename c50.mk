d0b250cf29504aa9098c9818ffb8b884ad61e622
chevan gard <chevanlol360@aol.com>
2015-07-02 14:08:40 -0400

    wifi (1/3)
    Signed-off-by: chevan gard <chevanlol360@aol.com>

--- c50.mk
+++ c50.mk
@@ -200,8 +200,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-#    device/lge/c50/wcnss/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-#    device/lge/c50/wcnss/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-#    device/lge/c50/wcnss/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-#    device/lge/c50/wcnss/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
-#    device/lge/c50/wcnss/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+    device/lge/c50/wcnss/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    device/lge/c50/wcnss/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    device/lge/c50/wcnss/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    device/lge/c50/wcnss/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+    device/lge/c50/wcnss/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    device/lge/c50/wcnss/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/lge/c50/wcnss/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf 
